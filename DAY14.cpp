36de9e2f280c3f3c46e0df6794f7dd4819f94752
Karan7856 <146857097+Karan7856@users.noreply.github.com>
2024-09-17 19:45:17 +0530

    Add files via upload

---
+++ DAY14.cpp
@@ -0,0 +1,42 @@
+//QUESTION 1
+//Define a car class with properties brand name , model, engine, number of seats and method displayInfo that display the all information of class.
+// #include <iostream>
+// using namespace std;
+// class Car{
+// public:
+//     string brand_name;
+//     string model;
+//     int no_of_seats;
+//     Car(string brand_name,string model,int no_of_seats){
+//         this->brand_name=brand_name;
+//         this->model=model;
+//         this->no_of_seats=no_of_seats;
+//     }
+
+
+// };
+// void Display_Info(Car c){
+//     cout<<c.brand_name<<endl;
+//     cout<<c.model<<endl;
+//     cout<<c.no_of_seats<<endl;
+// }
+// int main(){
+//     Car car1("Honda","SUV",10);
+//     Display_Info(car1);
+
+// }
+
+
+//QUESTION 2
+
+//Write a program that defines a Rectangle class with a copy constructor
+
+#include <iostream>
+using namespace std;
+class Rectangle{
+public:
+    
+}
+int main(){
+
+}
